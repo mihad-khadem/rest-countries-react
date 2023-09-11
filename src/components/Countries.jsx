a10e3ba195b6058b9908099e9fa464edeeb04594
mihad-khadem <mihadkhadem@gmail.com>
2023-09-11 08:13:31 +0600

    Flag initialized

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -15,7 +15,7 @@ const Countries = () => {
         <div>
             <h2>Country : {countries.length} </h2>
             {
-                countries.map(country => <Country country={country}></Country>)
+                countries.map(country => <Country key={country.cca3} country={country}></Country>)
             }
             
         </div>
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,8 +1,10 @@
+import './country.css';
 const Country = ({country}) => {
-    const {name} = country
+    const {name, flags} = country
     console.log(country);
     return (
-        <div>
+        <div className="box">
+            <img src={flags.png} alt="flag" />
             <h2>Name : {name.common}</h2>
         </div>
     );
